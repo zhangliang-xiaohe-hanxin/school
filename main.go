1324ab8d3b21a19cb77fa35b89c3f2cbefc2b05c
Ball (Thanat) <thanat.arp@gmail.com>
2019-06-22 17:14:39 +0700

    port

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/hanxin/school/database"
+	"fmt"
+	"os"
 )
 
 func main() {
@@ -11,6 +13,6 @@ func main() {
 	r.GET("/api/todos", database.GetTodosHandler)
 	r.POST("/api/todos", database.InsertDB)
 	r.DELETE("/api/todos/:id", database.DeleteTodo)
-	r.Run(":80")
+	r.Run(fmt.Sprintf(":", os.Getenv("port")))
 
 }
