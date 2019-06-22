7cb513c08f7fcb147e3a03c658abd8fc5911d690
Ball (Thanat) <thanat.arp@gmail.com>
2019-06-22 16:12:04 +0700

    add Post method

---
+++ database/post.go
@@ -0,0 +1,16 @@
+package database 
+
+import (
+	// "database/sql"
+	// "log"
+	// "net/http"
+	// "os"
+	// "strconv"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+)
+
+func InsertDB(c *gin.Context) {
+	
+}
